2b424ba25400d08d466ee7aed22c48eb8684328d
ReiL-RL <77379303+ReiL-RL@users.noreply.github.com>
2023-04-28 22:29:25 +0500

    authorezation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ToureCardComponent } from './component/toure-card/toure-card.component';
+import { ResetPaswordComponent } from './component/reset-pasword/reset-pasword.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToureCardComponent
+    ToureCardComponent,
+    ResetPaswordComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
