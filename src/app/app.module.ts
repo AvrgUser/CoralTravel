028116e59416ca73899f3802dac3c905a5baf74e
Valeriy Bulak <87676156+AvrgUser@users.noreply.github.com>
2023-04-28 20:59:50 +0300

    в

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { Component, NgModule } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 
 import { AppComponent } from './app.component';
 import { ToureCardComponent } from './component/toure-card/toure-card.component';
-
+import * as coral_api from '../api';
 
 @NgModule({
   declarations: [
@@ -18,6 +18,12 @@ import { ToureCardComponent } from './component/toure-card/toure-card.component'
     AppComponent,
   ],
   providers: [],
+  
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule implements OnInit {
+  
+  ngOnInit(){
+    console.log(coral_api.GetCities())
+  }
+}
