8051c4b3a6dc92649e41a077dce51ebf93d11e3c
Valeriy Bulak <87676156+AvrgUser@users.noreply.github.com>
2023-05-07 18:24:26 +0300

    db connection

--- backend/db-connection/db.connector.js
+++ backend/db-connection/db.connector.js
@@ -1,14 +1,50 @@
-let sql
+let sql = require('mysql').createConnection({
+    host: '109.248.206.65:3306',
+    user: 'u174_hDiLTbWKdf',
+    password: 'zlAST5kS2Cm.B!n1tLV.HM!G',
+    database: 's174_ReiL',
+})
+
+function sendQuery(board, column, callback){
+    sendQuery(board, column, undefined, callback)
+}
+
+function sendQuery(board, column, params, callback){
+    let query = `SELECT ${column} FROM ${board}`
+    if(params!=undefined){
+        query+=' WHERE '
+        for(let i=0;i<params.length;i++){
+            query += `${params[i].name} = ${params[i].value}`
+            if(i<params.length-1) query +=' AND '
+        }
+    }
+    return sql.query(query, callback)
+}
 
 module.exports = {
-    queries : {
-        cities: '',
-        clients: '',
-        orders: '',
+    getCitiesList(){
 
+        return 'Moscow Saint-Petersburg Bryansk'
     },
 
-    getCitiesList(){
-        return 'Moscow Saint-Petersburg Bryansk'
-    }
+    getToursList(params){
+    },
+
+    getClientsList(callback){
+        sendQuery('clients', '*', callback)
+    },
+
+    getClientInfo(login, callback){
+        this.getClientInfo(login, '*', callback)
+    },
+
+    getClientInfo(login, column, callback){
+        sendQuery('clients', column, [
+            {
+                name: 'login',
+                value: login
+            }
+        ],
+        callback)
+    },
 }
--- backend/main.js
+++ backend/main.js
@@ -1,23 +1,30 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const path = require('path')
 const dbconnector = require('./db-connection/db.connector')
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 // Указываем папку, в которой находятся статические файлы сайта (html, css, js и т.д.)
-app.use(express.static(path.resolve(__dirname, '..')+'/dist/coral-travel'));
+app.use(express.static(path.resolve(__dirname, '..')+'/dist/coral-travel'))
 
 // Обработка GET-запроса на главную страницу
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..')+'/dist/coral-travel/index.html');
-});
+  res.sendFile(path.resolve(__dirname, '..')+'/dist/coral-travel/index.html')
+})
 
 app.get('/cities',(req, res) => {
   res.setHeader('Content-Type', 'text/plain')
   res.end(dbconnector.getCitiesList())
-});
+})
+
+app.get('/users',(request, response) => {
+  response.setHeader('Content-Type', 'text/plain')
+  dbconnector.getUsersList((result, error)=>{
+    response.end(result)
+  })
+})
 
 // Запускаем сервер на порту 3000
 app.listen(3000, () => {
-  console.log('Сервер запущен: http://localhost:3000');
-});
+  console.log('Сервер запущен: http://localhost:3000')
+})
