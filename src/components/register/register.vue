d7024ffd39f1073b4346de82bc59e6b42decd500
ReiL-RL <77379303+ReiL-RL@users.noreply.github.com>
2023-05-30 21:23:27 +0500

    account

--- src/components/register/register.vue
+++ src/components/register/register.vue
@@ -64,30 +64,30 @@
     methods:{
       logup(){
         const loginR_ = document.getElementById('loginR')
-    const firstName_ = document.getElementById('firstName')
-    const lastName_ = document.getElementById('lastName')
-    const email_ = document.getElementById('email')
-    const passwordR_ = document.getElementById('passwordR')
-    const politics_ = document.getElementById('politics')
-    const infoR_ = document.getElementById('infoR')
+        const firstName_ = document.getElementById('firstName')
+        const lastName_ = document.getElementById('lastName')
+        const email_ = document.getElementById('email')
+        const passwordR_ = document.getElementById('passwordR')
+        const politics_ = document.getElementById('politics')
+        const infoR_ = document.getElementById('infoR')
 
-    if(loginR_.value == ""){ infoR_.textContent = "Введите логин!";  return; }
-    if(firstName_.value == "") { infoR_.textContent = "Введите имя!"; return; }
-    if(lastName_.value == ""){ infoR_.textContent = "Введите фамилию!";  return; }
-    if(email_.value == "") { infoR_.textContent = "Введите адрес элетронной почты!"; return; }
-    if(passwordR_.value == "") { infoR_.textContent = "Введите пороль!"; return; }
-    if(!politics_.checked) { infoR_.textContent = "Примите соглашение"; return; }
+        if(loginR_.value == ""){ infoR_.textContent = "Введите логин!";  return; }
+        if(firstName_.value == "") { infoR_.textContent = "Введите имя!"; return; }
+        if(lastName_.value == ""){ infoR_.textContent = "Введите фамилию!";  return; }
+        if(email_.value == "") { infoR_.textContent = "Введите адрес элетронной почты!"; return; }
+        if(passwordR_.value == "") { infoR_.textContent = "Введите пороль!"; return; }
+        if(!politics_.checked) { infoR_.textContent = "Примите соглашение"; return; }
 
-    Api.LogUp(loginR_.value, passwordR_.value).then(response=>{
-      if(response.result=='success') {
-        User.isAuth = true
-        disableForms('exampleModalToggle2');
-        Cookie.set('login', loginR_.value)
-        Cookie.set('password', passwordR_.value)
-        this.$forceUpdate();
-      }
-      else infoR_.textContent = "fail!";
-    })
+        Api.LogUp(loginR_.value, passwordR_.value).then(response=>{
+          if(response.result=='success') {
+            User.isAuth = true
+            disableForms('exampleModalToggle2');
+            Cookie.set('login', loginR_.value)
+            Cookie.set('password', passwordR_.value)
+            this.$forceUpdate();
+          }
+          else infoR_.textContent = "fail!";
+        })
       }
     },
     components: { 
