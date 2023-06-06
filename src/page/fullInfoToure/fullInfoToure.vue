3aff59211fa6a0211946c86bda8fa5d9ea132ace
ReiL-RL <77379303+ReiL-RL@users.noreply.github.com>
2023-06-06 22:31:16 +0500

    fullinfo

--- src/page/fullInfoToure/fullInfoToure.vue
+++ src/page/fullInfoToure/fullInfoToure.vue
@@ -10,8 +10,8 @@
             </div>
           </div>
         </div>
-      </nav>
-      <div class="section1">
+    </nav>
+    <div class="section1">
         <div class="row">
             <div class="title">
                 <h4>OTIUM FAMILY ECO CLUB</h4>
@@ -72,24 +72,116 @@
                         Качество обслуживания под контролем Otium Hotels и Coral Travel.
                     </p>
                     <div class="featuresWrap ">
+                        <div class="features-item" data-bs-toggle="tooltip" data-bs-placement="top" title="Детское меню в ресторане">
+                            <img src="@/assets/comfort/menu.svg" alt="">
+                        </div>
+                        <div class="features-item" data-bs-toggle="tooltip" data-bs-placement="top" title="Детские бассейны">
+                            <img src="@/assets/comfort/pool.svg" alt="">
+                        </div>
+                        <div class="features-item" data-bs-toggle="tooltip" data-bs-placement="top" title="Дискотека">
+                            <img src="@/assets/comfort/disco.svg" alt="">
+                        </div>
+                        <div class="features-item" data-bs-toggle="tooltip" data-bs-placement="top" title="Крытый Бассейн">
+                            <img src="@/assets/comfort/Indoor-pool.svg" alt="">
+                        </div>
+                        <div class="features-item" data-bs-toggle="tooltip" data-bs-placement="top" title="Мини-Клуб">
+                            <img src="@/assets/comfort/mini-club.svg" alt="">
+                        </div>
+                        <div class="features-item" data-bs-toggle="tooltip" data-bs-placement="top" title="Услуги няни">
+                            <img src="@/assets/comfort/heated-swimming-pool.svg" alt="">
+                        </div>
+                        <div class="features-item" data-bs-toggle="tooltip" data-bs-placement="top" title="Бассейн с подогревом">
+                            <img src="@/assets/comfort/pool_88580.svg" alt="">
+                        </div>
+                        <div class="features-item" data-bs-toggle="tooltip" data-bs-placement="top" title="Наличие аквапарка">
+                            <img src="@/assets/comfort/aquapark.svg" alt="">
+                        </div>
+                        <div class="features-item" data-bs-toggle="tooltip" data-bs-placement="top" title="Детский аквапарк">
+                            <img src="@/assets/comfort/childrens-aquapark.svg" alt="">
+                        </div>
+                        <div class="features-item" data-bs-toggle="tooltip" data-bs-placement="top" title="Wi-Fi">
+                            <img src="@/assets/comfort/wi-fi.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="buy">
+                        <button type="button" class="btn btn-outline-primary">Забронировать</button>
+                        <button type="button" class="btn btn-outline-primary">Купить</button>
+                    </div>
+                </div>
+            </div>
+            <div class="fullInfo">
+                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-general" type="button" role="tab" aria-controls="pills-general" aria-selected="true">Общее</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-services" type="button" role="tab" aria-controls="pills-services" aria-selected="false">Услуги</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-rooms" type="button" role="tab" aria-controls="pills-rooms" aria-selected="false">Номера</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#pills-food" type="button" role="tab" aria-controls="pills-food" aria-selected="false">Еда и напитки</button>
+                    </li>
 
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#pills-concept" type="button" role="tab" aria-controls="pills-concept" aria-selected="false">Концепция</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#pills-restaurants" type="button" role="tab" aria-controls="pills-restaurants" aria-selected="false">Рестораны</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#pills-bars" type="button" role="tab" aria-controls="pills-bars" aria-selected="false">Бары</button>
+                    </li>
+                </ul>
+                <div class="tab-content" id="pills-tabContent">
+                    <div class="tab-pane fade show active" id="pills-general" role="tabpanel" aria-labelledby="pills-general-tab" tabindex="0">
+                        general
+                    </div>
+
+                    <div class="tab-pane fade" id="pills-services" role="tabpanel" aria-labelledby="pills-services-tab" tabindex="0">
+                        services
+                    </div>
+
+                    <div class="tab-pane fade" id="pills-rooms" role="tabpanel" aria-labelledby="pills-rooms-tab" tabindex="0">
+                        rooms
+                    </div>
+
+                    <div class="tab-pane fade" id="pills-food" role="tabpanel" aria-labelledby="pills-food-tab" tabindex="0">
+                        food
+                    </div>
+
+                    <div class="tab-pane fade" id="pills-concept" role="tabpanel" aria-labelledby="pills-concept-tab" tabindex="0">
+                        concept
+                    </div>
+
+                    <div class="tab-pane fade" id="pills-restaurants" role="tabpanel" aria-labelledby="pills-restaurants-tab" tabindex="0">
+                        restaurants
+                    </div>
+
+                    <div class="tab-pane fade" id="pills-bars" role="tabpanel" aria-labelledby="pills-bars-tab" tabindex="0">
+                        bars
                     </div>
                 </div>
             </div>
         </div>
-      </div>
+    </div>
+    <footerComponent></footerComponent>
   </template>
     
-    <script lang="ts">
-  
+<script lang="ts">
+    import footerComponent from '@/components/footer/footerComponent.vue';    
     export default {
       name: "fullInfoToure",
       data(){
       return{
         isAuth: false,
+
+
       }
     },
       components: {
+        footerComponent,
       }
     }
     </script>
